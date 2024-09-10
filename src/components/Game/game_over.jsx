c142342cec9fc90d4736fe5ba91509d567c8e01e
KrishitaGarg <krishitagarg@gmail.com>
2024-09-10 13:30:54 +0530

    Updated game over page

--- src/components/Game/game_over.jsx
+++ src/components/Game/game_over.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import titleImage from "../../assets/game_over.png";
-import styles from "./game_over.css";
-
+import gameOverImage from "../../assets/game_over.png";
+import "./game_over.css"
 const GameOverComponent = () => {
   return (
-    <div className={styles.container}>
-      <img src={titleImage} alt="Game Over" className={styles.titleImage} />
+    <section className="game-over">
+      <img src={gameOverImage} alt="Game Over" className="gameOverImage" />
+      <Link to="/startgame" style={{ textDecoration: "none" }}>
+        <button className="playAgainButton">
+          Play <span>Again</span>
+        </button>
+      </Link>
       <Link to="/" style={{ textDecoration: "none" }}>
-        <button className={styles.homeButton}>
+        <button className="homeButton">
           Go back to <span>Home</span>
         </button>
       </Link>
-    </div>
+    </section>
   );
 };
 
